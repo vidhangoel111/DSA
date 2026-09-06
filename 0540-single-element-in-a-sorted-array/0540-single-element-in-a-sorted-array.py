faab51ebb66973f57d1c07d4a819307ce876f052
Vidhan Goel <vidhan.goel.ssgn4364@gmail.com>
2026-09-06 17:19:35 +0530

    Time: 0 ms (100%), Space: 16.9 MB (35.07%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
@@ -3,15 +3,21 @@ class Solution(object):
         n = len(nums)
         if(n==1):
             return nums[0]
-        for i in range(n):
-            if(i==0):
-                if(nums[i]!=nums[i+1]):
-                    return nums[i]
-            elif(i==n-1):
-                if(nums[i]!=nums[i-1]):
-                    return nums[i]
+        if(nums[0]!=nums[1]):
+            return nums[0]
+        if(nums[n-1]!=nums[n-2]):
+            return nums[n-1]
+        
+        low = 1
+        high = n-2
+        while(low<=high):
+            mid = (low + high)//2
+            if(nums[mid]!=nums[mid+1] and nums[mid]!=nums[mid-1]):
+                return nums[mid]
+            
+            if((mid%2==1 and nums[mid-1]==nums[mid]) or (mid%2==0 and nums[mid]==nums[mid+1])):
+                low = mid + 1
             else:
-                if(nums[i]!=nums[i+1] and nums[i]!=nums[i-1]):
-                    return nums[i]
-
+                high = mid - 1
+        return -1
         
